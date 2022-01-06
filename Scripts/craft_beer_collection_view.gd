bbdfcb0e9dc7023bc8672bed5572c580644971ca
Bernie Mayer <bernie.mayer@clio.com>
2022-01-05 18:47:34 -0700

    Change starting scene and fix dependencies

--- Scripts/craft_beer_collection_view.gd
+++ Scripts/craft_beer_collection_view.gd
@@ -1,25 +1,12 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_BeerButton_pressed():
 	if get_tree().change_scene("res://Scenes/BeerDescriptionGUI.tscn") != OK:
-		print("An expected error occured when trying to view a Beer")
-
+		print("An expected error occured when trying to view a Beer")\
 
 func _on_BrewButton_pressed():
 	if get_tree().change_scene("res://Scenes/CraftBeerBrewingView.tscn") != OK:
